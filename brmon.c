cdb488f99d52529ea51665c60868336910bd508c
Jonas Gorski <jonas.gorski@gmail.com>
2026-05-31 16:08:30 +0200

    convert to libmnl

--- brmon.c
+++ brmon.c
@@ -16,6 +16,7 @@
 #include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <fcntl.h>
 #include <netinet/in.h>
 #include <linux/if_bridge.h>
@@ -60,152 +61,72 @@ static struct mnl_socket *mnl;
 static struct epoll_event_handler br_handler;
 
 static struct mnl_socket *mnl_state;
+static int state_seq;
+
+static int mnl_talk(struct mnl_socket *nl, struct nlmsghdr *msg,
+		    struct nlmsghdr **answer)
+{
+    char buf[MNL_SOCKET_BUFFER_SIZE];
+    unsigned int seq = ++state_seq;
+    int ret;
+
+    msg->nlmsg_seq = seq;
+
+    if (mnl_socket_sendto(nl, msg, msg->nlmsg_len) < 0)
+    {
+        ERROR("mnl_socket_sendto failed: %m");
+	return -1;
+    }
+
+    ret = mnl_socket_recvfrom(nl, buf, sizeof(*buf));
+    if (ret < 0)
+    {
+        ERROR("mnl_socket_recvfrom failed: %m");
+	return -1;
+    }
+
+    ret = mnl_cb_run(buf, ret, seq, mnl_socket_get_portid(nl), NULL, NULL);
+    if (ret < 0)
+    {
+        ERROR("mnl_cb_run failed: %m");
+	return -1;
+    }
+
+    if (answer) {
+        *answer = malloc(msg->nlmsg_len);
+	memcpy(*answer, buf, msg->nlmsg_len);
+    }
+
+    return 0;
+}
 
 int br_set_state(unsigned ifindex, __u8 state)
 {
-#if 0	
-    struct
-    {
-        struct nlmsghdr n;
-        struct ifinfomsg ifi;
-        char buf[256];
-    } req;
+    char buf[MNL_SOCKET_BUFFER_SIZE];
+    struct ifinfomsg *ifi;
+    struct nlmsghdr *n;
 
-    memset(&req, 0, sizeof(req));
+    n = mnl_nlmsg_put_header(buf);
+    n->nlmsg_type = RTM_SETLINK;
+    n->nlmsg_flags = NLM_F_REQUEST | NLM_F_REPLACE;
+    ifi = mnl_nlmsg_put_extra_header(n, sizeof(*ifi));
+    ifi->ifi_family = AF_BRIDGE;
+    ifi->ifi_index = ifindex;
 
-    req.n.nlmsg_len = NLMSG_LENGTH(sizeof(struct ifinfomsg));
-    req.n.nlmsg_flags = NLM_F_REQUEST | NLM_F_REPLACE;
-    req.n.nlmsg_type = RTM_SETLINK;
-    req.ifi.ifi_family = AF_BRIDGE;
-    req.ifi.ifi_index = ifindex;
+    mnl_attr_put_u8(n, IFLA_PROTINFO, state);
 
-    addattr8(&req.n, sizeof(req.buf), IFLA_PROTINFO, state);
-
-    return rtnl_talk(&rth_state, &req.n, NULL);
-#endif
-    return 0;
+    return mnl_talk(mnl_state, n, NULL);
 }
-#if 0
-static int listen_msg(struct rtnl_ctrl_data *data, struct nlmsghdr *n,
-                    void *arg)
+
+static const enum mnl_attr_data_type link_policy[IFLA_MAX + 1] =
 {
-    struct ifinfomsg *ifi = NLMSG_DATA(n);
-    struct rtattr * tb[IFLA_MAX + 1];
-    int len = n->nlmsg_len;
-    char b1[IFNAMSIZ];
-    int af_family;
-    bool newlink;
-    int br_index;
-
-    if(n->nlmsg_type == NLMSG_DONE)
-        return 0;
-
-    len -= NLMSG_LENGTH(sizeof(*ifi));
-    if(len < 0)
-    {
-        return -1;
-    }
-
-    af_family = ifi->ifi_family;
-
-    if(af_family != AF_BRIDGE && af_family != AF_UNSPEC)
-        return 0;
-
-    if(n->nlmsg_type != RTM_NEWLINK && n->nlmsg_type != RTM_DELLINK)
-        return 0;
-
-    parse_rtattr(tb, IFLA_MAX, IFLA_RTA(ifi), len);
-
-    /* Check if we got this from bonding */
-    if(tb[IFLA_MASTER] && af_family != AF_BRIDGE)
-        return 0;
-
-    if(tb[IFLA_IFNAME] == NULL)
-    {
-        ERROR("BUG: nil ifname");
-        return -1;
-    }
-
-    if(n->nlmsg_type == RTM_DELLINK)
-        LOG("Deleted ");
-
-    LOG("%d: %s ", ifi->ifi_index, (char*)RTA_DATA(tb[IFLA_IFNAME]));
-
-    if(tb[IFLA_OPERSTATE])
-    {
-        __u8 state = *(__u8*)RTA_DATA(tb[IFLA_OPERSTATE]);
-        switch (state)
-        {
-            case IF_OPER_UNKNOWN:
-                LOG("Unknown ");
-                break;
-            case IF_OPER_NOTPRESENT:
-                LOG("Not Present ");
-                break;
-            case IF_OPER_DOWN:
-                LOG("Down ");
-                break;
-            case IF_OPER_LOWERLAYERDOWN:
-                LOG("Lowerlayerdown ");
-                break;
-            case IF_OPER_TESTING:
-                LOG("Testing ");
-                break;
-            case IF_OPER_DORMANT:
-                LOG("Dormant ");
-                break;
-            case IF_OPER_UP:
-                LOG("Up ");
-                break;
-            default:
-                LOG("State(%d) ", state);
-        }
-    }
-
-    if(tb[IFLA_MTU])
-        LOG("mtu %u ", *(int*)RTA_DATA(tb[IFLA_MTU]));
-
-    if(tb[IFLA_MASTER])
-    {
-        LOG("master %s ",
-                if_indextoname(*(int*)RTA_DATA(tb[IFLA_MASTER]), b1));
-    }
-
-    if(tb[IFLA_PROTINFO])
-    {
-        uint8_t state = *(uint8_t *)RTA_DATA(tb[IFLA_PROTINFO]);
-        if(state <= BR_STATE_BLOCKING)
-            LOG("state %s", port_states[state]);
-        else
-            LOG("state (%d)", state);
-    }
-
-    newlink = (n->nlmsg_type == RTM_NEWLINK);
-
-    if(tb[IFLA_MASTER])
-        br_index = *(int*)RTA_DATA(tb[IFLA_MASTER]);
-    else if(is_bridge((char*)RTA_DATA(tb[IFLA_IFNAME])))
-        br_index = ifi->ifi_index;
-    else
-        br_index = -1;
-
-    bridge_notify(br_index, ifi->ifi_index, newlink, ifi->ifi_flags);
-
-    return 0;
-}
-static int dump_msg(struct nlmsghdr *n, void *arg)
-{
-    return listen_msg(NULL, n, arg);
-}
-
-static inline void br_ev_handler(uint32_t events, struct epoll_event_handler *h)
-{
-    if(rtnl_listen(&rth, listen_msg, stdout) < 0)
-    {
-        ERROR("Error on bridge monitoring socket");
-    }
-}
-#endif
+    [IFLA_ADDRESS] = MNL_TYPE_BINARY,
+    [IFLA_IFNAME] = MNL_TYPE_STRING,
+    [IFLA_MTU] = MNL_TYPE_U32,
+    [IFLA_MASTER] = MNL_TYPE_U32,
+    [IFLA_PROTINFO] = MNL_TYPE_U8,
+    [IFLA_OPERSTATE] = MNL_TYPE_U8,
+};
 
 static int link_attr_cb(const struct nlattr *attr, void *data)
 {
@@ -215,20 +136,8 @@ static int link_attr_cb(const struct nlattr *attr, void *data)
     if (mnl_attr_type_valid(attr, IFLA_MAX) < 0)
 	    return MNL_CB_OK;
 
-    switch(type) {
-	    case IFLA_ADDRESS:
-		    if (mnl_attr_validate(attr, MNL_TYPE_BINARY) < 0)
-			    return MNL_CB_ERROR;
-		    break;
-	    case IFLA_MTU:
-		    if (mnl_attr_validate(attr, MNL_TYPE_U32) < 0)
-			    return MNL_CB_ERROR;
-		    break;
-	    case IFLA_IFNAME:
-		    if (mnl_attr_validate(attr, MNL_TYPE_STRING) < 0)
-			    return MNL_CB_ERROR;
-		    break;
-    }
+    if (mnl_attr_validate(attr, link_policy[type]) < 0)
+	    return MNL_CB_ERROR;
 
     tb[type] = attr;
     return MNL_CB_OK;
@@ -237,7 +146,7 @@ static int link_attr_cb(const struct nlattr *attr, void *data)
 static int link_cb(const struct nlmsghdr *n, void *data)
 {
     struct ifinfomsg *ifi = NLMSG_DATA(n);
-    struct nlattr * tb[IFLA_MAX + 1];
+    struct nlattr * tb[IFLA_MAX + 1] = { };
     int len = n->nlmsg_len;
     char b1[IFNAMSIZ];
     int af_family;
@@ -352,12 +261,10 @@ static inline void br_ev_handler(uint32_t events, struct epoll_event_handler *h)
     while (ret > 0)
     {
         ret = mnl_cb_run(buf, ret, 0, 0, link_cb, NULL);
-	INFO("br_ev_handler: mnl_cb_run() = %i", ret);
 	if (ret <= MNL_CB_STOP)
 		break;
 
 	ret = mnl_socket_recvfrom(mnl, buf, sizeof(buf));
-	INFO("br_ev_handler: mnl_socket_recvfrom() = %i", ret);
     }
 
     if(ret == -1)
@@ -394,12 +301,10 @@ static int br_linkdump(struct mnl_socket *mnl, int family)
     while (ret > 0)
     {
         ret = mnl_cb_run(buf, ret, seq, portid, link_cb, NULL);
-	INFO("br_linkdump: mnl_cb_run() = %i", ret);
 	if (ret <= MNL_CB_STOP)
 		break;
 
 	ret = mnl_socket_recvfrom(mnl, buf, sizeof(buf));
-	INFO("br_linkdump: mnl_socket_recvfrom() = %i", ret);
     }
 
     if (ret == -1)
@@ -443,13 +348,14 @@ int init_bridge_ops(void)
         return -1;
     }
 #if 1
-    if(br_linkdump(mnl_state, PF_BRIDGE) < 0)
+    if(br_linkdump(mnl, PF_BRIDGE) < 0)
     {
         ERROR("Cannot send dump request: %m");
         return -1;
     }
 
     fd = mnl_socket_get_fd(mnl);
+    state_seq = time(NULL);
 
 #else
     if(rtnl_linkdump_req(&rth, PF_BRIDGE) < 0)
